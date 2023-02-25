2566baea89786e3c77c4d8013d353b8730cd820c
Jason Aucordier <jaucordi@gmail.com>
2023-02-25 07:03:44 +0100

    Adding 'header' mode to UserAccount react component

--- front-end/src/components/UserAccount/UserAccount.tsx
+++ front-end/src/components/UserAccount/UserAccount.tsx
@@ -1,44 +1,21 @@
+import type {BankAccount} from "../../services/openapi";
 import {useNavigate} from "react-router-dom";
 import {Card} from "../Card/Card";
 
-interface IAccount {
-    id: string;
-    type: string;
-    ref: string;
-    title: string;
-    description: string;
-    amount: {
-        currency: string,
-        value: number,
-    },
-}
-
-interface ICard {
-    id: string;
-    type: string;
-    ref: string;
-    title: string;
-    description: string;
-    amount: {
-        currency: string,
-        value: number,
-    },
-}
-
-type IUserAccount = IAccount | ICard;
-
 
 type UserAccountProps = {
-    data: IUserAccount;
+    data: BankAccount;
+    header?: boolean;
 };
 
-function UserAccount({data}: UserAccountProps) {
+function UserAccount({data, header = false}: UserAccountProps) {
     const navigate = useNavigate();
 
-    return <Card title={`${data.title} (${data.ref})`}
-                 amount={`${data.amount.currency}${data.amount.value.toLocaleString()}`}
+    return <Card title={`${data.name} (${data.ref})`}
+                 amount={`${data.amount.value.toLocaleString('en-US', {style: 'currency', currency: data.amount.currency, minimumFractionDigits: 2})}`}
                  amountDescription={data.description}
-                 button={{text: 'View transactions', onClick: () => navigate(`/account/${data.id}`)}} />;
+                 banner={header}
+                 button={!header ? {text: 'View transactions', onClick: () => navigate(`/account/${data.id}`)} : undefined} />;
 }
 
 export type {UserAccountProps}
