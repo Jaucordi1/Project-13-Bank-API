23c4ccaa3ebb35fd8496477c0cd3faa8cf07b9ee
Jason Aucordier <jaucordi@gmail.com>
2023-02-25 07:03:31 +0100

    Adding 'banner' mode to Card react component

--- front-end/src/components/Card/Card.tsx
+++ front-end/src/components/Card/Card.tsx
@@ -1,11 +1,12 @@
 import Classes from './Card.module.css';
-import classNames from "classnames";
 import {Button} from "../Button/Button";
+import classNames from "classnames";
 
 export type CardProps = {
     title: string;
     amount: string;
     amountDescription: string;
+    banner?: boolean;
     button?: {
         text: string;
         onClick: () => void;
@@ -13,21 +14,24 @@ export type CardProps = {
 };
 
 export function Card(props: CardProps) {
-    const {title, amount, amountDescription, button} = props;
+    const {title, amount, amountDescription, button, banner = false} = props;
 
     return (
-        <section className={Classes.wrapper}>
+        <section className={classNames(Classes.container, {[Classes.header]: banner})}>
             <div className={Classes.content}>
                 <h3 className={Classes.title}>{title}</h3>
-                <p className={Classes.amount}>
-                    {/*$2,082.79*/}
-                    {amount}
+                <p className={Classes.amount}>{amount}</p>
+                <p className={Classes.amountDescription}>
+                    {amountDescription}
                 </p>
-                <p className={Classes.amountDescription}>{amountDescription}</p>
             </div>
-            {button && <div className={classNames(Classes.content, Classes.cta)}>
-                <Button className={Classes.button} onClick={button.onClick}>{button.text}</Button>
-            </div>}
+            {button && (
+                <div className={classNames(Classes.content, Classes.cta)}>
+                    <Button className={Classes.button} onClick={button.onClick}>
+                        {button.text}
+                    </Button>
+                </div>
+            )}
         </section>
     );
 }
